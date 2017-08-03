85c77318cac723c4ae6543ed1a47e41f245d4c17
GitanjaliMazumdar <mazumdargitanjali@gmail.com>
2017-08-03 19:54:43 +0530

    Update cachematrix.R

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,62 @@
-## Put comments here that give an overall description of what your
+
+## The code consists of functions which cahces the inverse of a matrix so that it need not be repeatedly 
+## calculated as matrix inversion is a costly computation 
 ## functions do
 
-## Write a short comment describing this function
+## The first function is the makeCacheMatrix function which creates a special matrix i.e. a list which sets
+## value of the matrix, gets its value then sets the value of its inverse & finally gets the inverse. 
 
 makeCacheMatrix <- function(x = matrix()) {
-
+  inv <- NULL
+  set <- function(y){
+          x <<- y
+          inv <<- NULL
+  }
+  get <- function() x
+  setInverse <- function(inverse) inv <<- inverse
+  getInverse <- function() inv
+  list(set=set, get=get, setInverse=setInverse, getInverse=getInverse)
 }
 
-
-## Write a short comment describing this function
+## The second function first checks that whether the matrix inverse has already been calculated if yes then 
+## gets the value from the cache & will not compute the same. If not then it will compute the matrix inverse
 
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+  inv<- x$getInverse()
+  if(!is.null(inv)){
+    message("getting cached data")
+    return(inv)
+  } 
+  else{
+    data <- x$get()
+    inv <- solve(data)
+    x$setInverse(inv)
+    inv
+  }
+  
 }
+
+# example along with the output
+# # using matrix x
+#   x <- matrix(1:4, nrow=2, ncol = 2)
+# # makeCacheMatrix function creates the special matrix
+#   z <- makeCacheMatrix(x)
+#   # printing the matrix
+#     z$get()
+#     [,1] [,2]
+#     [1,]    1    3
+#     [2,]    2    4
+#   # its inverse should be NULL
+#     z$getInverse()
+#     NULL
+# # cacheSolve function computes the inverse matrix in the first run
+#   cacheSolve(z)
+#   [,1] [,2]
+#   [1,]   -2  1.5
+#   [2,]    1 -0.5
+#   # In the second run it will return the cached value
+#   cacheSolve(z)
+#   getting cached data
+#   [,1] [,2]
+#   [1,]   -2  1.5
+#   [2,]    1 -0.5
